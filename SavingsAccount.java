b34616ae56bc74ecbc645238d03c450470903865
YounesZ <ZYounes1900@outlook.com>
2026-02-06 13:32:30 -0500

    Changed if statements to switch WIP

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,4 +1,3 @@
-//prototype
 //The goal here is to create setters and getters with a constructor.
 //Savings Account: is where you hold on to your money and in a while you can get interest for holding that money within a specific time period.
 public class SavingsAccount
@@ -20,7 +19,7 @@ public class SavingsAccount
     }
     
     public double getSavings() //this will return savings.
-    { //Might be subject to change
+    { 
         return savings;
     }
     public void setSavings(double savings) //This will set Savings the private field to the parameter savings.
@@ -29,21 +28,19 @@ public class SavingsAccount
     }
     public double withdrawSavings(double amt, char choice) //withdraw system that records the amount.
     {
-        if(choice == "Checking") //This is going to change the intent for this is if the user picks "Checking" then subtract savings from the amt that was placed and the checking account managers add that amt value otherwise if "Savings" we'll add the value from amt and the checking account people would just subtract on their part.
-        {
-            if(savings >= amt)
-            {
-            return savings -= amt; 
-            }
-            else{
-                System.out.println("Amount is high.");
-                return savings; //this is going to be here temporarily.
-            } 
-        }
-        else if(choice == "Savings")
-        {
-            //TODO
-
+        switch(choice) //This is going to change the intent for this is if the user picks "Checking" then subtract savings from the amt that was placed and the checking account managers add that amt value otherwise if "Savings" we'll add the value from amt and the checking account people would just subtract on their part.
+        {   
+            case "Checking":
+                if(savings >= amt){
+                    return savings -= amt; 
+                }
+                else{
+                    System.out.println("Amount is high.");
+                    return savings; //this is going to be here temporarily.
+                }
+            break;
+            case "Savings":
+                //Work in progress.
         }
     }
 
