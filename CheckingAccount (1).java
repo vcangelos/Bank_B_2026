ffda744958ca8df643f863d028e039c154d159b0
elizabethtchia <lizchiax@gmail.com>
2026-02-06 08:59:42 -0500

    Update CheckingAccount (1).java

--- CheckingAccount (1).java
+++ CheckingAccount (1).java
@@ -1,10 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class CheckingAccount {
     public getBalance(int AccountNum){
     //variables
     private double balance; 
     private double overdraftFee = 15;
     private double minBalanceFee = 100;
-    public 
+    public transactionHistory; 
+    
     //deposit
     public void deposit(int amt){
         balance += amt;
@@ -32,7 +37,14 @@ public class CheckingAccount {
     //minimum balance
     public void checkMinimumBalance()
         if (balance < minimumBalance){
-            savings -= minBalanceFee
+            savings -= minBalanceFee;
         }
-    //transaction history
-    public transactHistory()
+    //transaction history (withdrawls, deposits, purchases, fees -> for each: date, amount, new balance)
+    public transactionHistory()
+    //use 2d array
+    
+    //check if as savings account, if not charge fee
+    //create checking account
+    //seaparate csv file for only checkings
+    //more than one checking account -> unique checking account num separate from account num
+    
