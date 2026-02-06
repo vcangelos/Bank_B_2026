fce40c0a35268f27ee380df49b1ab8e8dbf0f334
Mustansar Farooq <97743639+MustansarFarooq@users.noreply.github.com>
2026-02-06 08:59:23 -0500

    Add files via upload
    Very basic, intro for CSV handling. Bank Menu is started while customer attributes were made. Will upload rest of CSV Handling ASAP.

---
+++ Customer.java
@@ -0,0 +1,29 @@
+
+public class Customer {
+  private String firstName;
+  private String lastName;
+  private int SSN;
+  private String DOB;
+  private String email;
+  private int phoneNumber;
+  private double minDeposit;
+
+
+
+
+
+
+
+    public void Customer()
+    {
+      //make csv for initializing customer or smth
+    }
+    //ok so input a username that we will pull from the csv
+    public void Customer(String userName)
+    {
+
+      //
+    }
+}
+
+
---
+++ bankMenu.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class bankMenu {
+
+
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("Login to Bank");
+
+    System.out.println("Username:");
+    String userName = keyboard.nextLine();
+    System.out.println("Password:");
+    String password = keyboard.nextLine();
+
+    //if username = from csv get by username
+    //then getting that information, get the password and compare the strings.
+    if()
+    {
+    }
+
+}
