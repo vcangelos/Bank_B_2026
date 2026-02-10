f094e6116791e0b41f7c217c8edb2623c91cb59e
jordinr16 <jrivera229@obps.org>
2026-02-10 08:57:51 -0500

    Add files via upload

--- BankLocation.java
+++ BankLocation.java
@@ -4,12 +4,12 @@ import java.util.Map;
 
 public class BankLocation {
     
-    //Franchise bank info
+    //Franchise-wide bank info
     private static String bankType = "Franchise Bank";
     private static double taxRate;
     private static double interestRate;
-    private String phoneNumber;
-    private String email;
+    private static String phoneNumber;
+    private static String email;
 
     //Map for all locations that stores key-value pairs
     private static Map<String, BankLocation> 
@@ -21,5 +21,70 @@ public class BankLocation {
     private String zipCode;
     private String routingNumber;
 
+    //Overloaded constructor that creates one location
+    public BankLocation(String branchName, String zipCode, String routingNumber) {
+        this.branchName;
+        this.zipCode;
+        this.routingNumber;
+        //Stores the location in the map
+        locations.put(zipCode, this);
+    }
+    
+    //Instance variable getters
+    public String getBranchName() 
+        { return branchName; }
+    
+    public String getZipCode()
+        { return zipCode; }
+    
+    public String getRoutingNumber() 
+        { return routingNumber; }
+    
+    //Static getters & setters
+    public static String getBankType() 
+        { return bankType; }
+
+    public static void setTaxRate(double rate)
+        { taxRate = rate; }
+    public static double getTaxRate() 
+        { return taxRate; }
+
+    public static void setInterestRate(double rate) 
+        { interestRate = rate; }
+    public static double getInterestRate()
+        { return interestRate; }
+
+    public static void setPhoneNumber(String phone) 
+        { phoneNumber = phone; }
+    public static String getPhoneNumber() 
+        { return phoneNumber; }
+
+    public static void setEmail(String em) 
+        { email = em; }
+    public static String getEmail() 
+        { return email; }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
+    
