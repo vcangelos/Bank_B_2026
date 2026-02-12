15c05a8f5be81b63e16a6ba2de7c1e3241530d6b
jordinr16 <jrivera229@obps.org>
2026-02-12 08:47:22 -0500

    Add files via upload

--- BankLocation.java
+++ BankLocation.java
@@ -14,7 +14,7 @@ public class BankLocation {
     //Map for all locations that stores key-value pairs
     private static Map<String, BankLocation> 
         locations = new HashMap<>();
-    //key-zip code, value-BankLocation object
+    //key: zip code, value: BankLocation object
     
     //Location specific info
     private String branchName;
@@ -23,48 +23,63 @@ public class BankLocation {
 
     //Overloaded constructor that creates one location
     public BankLocation(String branchName, String zipCode, String routingNumber) {
-        this.branchName;
-        this.zipCode;
-        this.routingNumber;
+        this.branchName = branchName;
+        this.zipCode = zipCode;
+        this.routingNumber = routingNumber;
         //Stores the location in the map
         locations.put(zipCode, this);
     }
     
     //Instance variable getters
-    public String getBranchName() 
-        { return branchName; }
+    public String getBranchName() {
+        return branchName; 
+    }
     
-    public String getZipCode()
-        { return zipCode; }
+    public String getZipCode() {
+        return zipCode; 
+    }
     
-    public String getRoutingNumber() 
-        { return routingNumber; }
+    public String getRoutingNumber()  {
+        return routingNumber; 
+    }
     
     //Static getters & setters
-    public static String getBankType() 
-        { return bankType; }
-
-    public static void setTaxRate(double rate)
-        { taxRate = rate; }
-    public static double getTaxRate() 
-        { return taxRate; }
-
-    public static void setInterestRate(double rate) 
-        { interestRate = rate; }
-    public static double getInterestRate()
-        { return interestRate; }
-
-    public static void setPhoneNumber(String phone) 
-        { phoneNumber = phone; }
-    public static String getPhoneNumber() 
-        { return phoneNumber; }
-
-    public static void setEmail(String em) 
-        { email = em; }
-    public static String getEmail() 
-        { return email; }
+    public static String getBankType() {
+        return bankType; 
+    }
     
+    public static double getTaxRate() {
+        return taxRate;
+    }
+    public static void setTaxRate(double rate) {
+        taxRate = rate; 
+    }
     
+    public static double getInterestRate() {
+        return interestRate; 
+    }
+    public static void setInterestRate(double rate) { 
+        interestRate = rate;
+    }
+    
+    public static String getPhoneNumber() {
+        return phoneNumber; 
+    }
+    public static void setPhoneNumber(String phone) {
+        phoneNumber = phone; 
+    }
+    
+    public static String getEmail() {
+        return email; 
+    }
+    public static void setEmail(String em) {
+        email = em; 
+    }
+    
+    //Allows access to the map and retrieval of specific locations
+    public static BankLocation getLocationByZip(String zip) {
+        return locations.get(zip);
+    }
     
     
     
