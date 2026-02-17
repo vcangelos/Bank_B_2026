31174815616bab4094c4aa6c517d16c91ff73eac
elizabethtchia <lizchiax@gmail.com>
2026-02-16 23:44:58 -0500

    Update CheckingAccount (1).java

--- CheckingAccount (1).java
+++ CheckingAccount (1).java
@@ -12,39 +12,94 @@ public class CheckingAccount {
     
     //deposit
     public void deposit(int amt){
-        balance += amt;
+        char acct = in.next().trim().toUpperCase().charAt(0); 
+        System.out.print("Amount: "); 
+        double amt = in.nextDouble(); 
+        if (amt <= 0) { // if amount entered is valid (<= 0)
+            System.out.println("Amount must be > 0."); 
+            continue;
+            } 
+            else if (amt > 0) { //if amount entered is valid (>0)
+                 balance += amt;
+            }
+            else {  //anything else entered that isn't valid (symbols,letters)
+            System.out.println("Invalid account."); 
+            continue; }
     }
+    
     //withdrawals
-    public boolean withdraw(int amt){
-        if (amt > balance)
-            if (hasOverdraftProtection == false){
-                balance -= overdraftFee;
+        char acct = in.next().trim().toUpperCase().charAt(0); //C is variable used for checking account 
+        System.out.print("Amount: "); 
+        double amt = int.nextDouble(); 
+        if (amt<= 0){
+            System.out.println("Amount must be > 0"); 
+        }
+        else if (acct == 'C' ) {
+        public void withdraw(int amt){
+            if (amt > balance){
+                if (hasOverdraftProtection == false){
+                    balance -= overdraftFee;
+                }
+                else if (hasOverdraftProtection == true){
+                    
+                    //finish code
+                }
+                else{
+                    left = amt - balance;
+                    savings -= left;
+                    balance = 0;
+                }
             }
             else{
-                left = amt - balance;
-                savings -= left;
-                balance = 0
-                
+               balance -= amt;  
+                System.out.print("Invalid Account."); 
             }
-        else{
-           balance -= amt;  
-        }    
-        
+        }}
 
+    public void loadSavingsStatus(csvFile csv) throws IOException { //check csv file
+
+        Map<String,String> record = csv.getRecord("AccountNumber", accountNumber); //checks for the row where the account number matches user's account
+
+        if (record != null) { //checks if account actually exists 
+            String value = record.get("HasSavings");// check w people for the csv file if column in csv file is named hasSavings. this checks that column
+
+            if (value != null && value.equalsIgnoreCase("true")) {// makes sure value exists. works for any capitalization or lower case letters if value is true.
+                hasSavingsAccount = true; //makes class boolean true 
+            } else { //anything other than true makes class false 
+                hasSavingsAccount = false;
+            }
+        } else {
+            hasSavingsAccount = false;
+        }
     }
+
 
         
     //minimum balance
-    public void checkMinimumBalance()
+    public void checkMinimumBalance();
         if (balance < minimumBalance){
             savings -= minBalanceFee;
         }
-    //transaction history (withdrawls, deposits, purchases, fees -> for each: date, amount, new balance)
-    public transactionHistory()
-    //use 2d array
+    //transaction history: use 2d array (withdrawls, deposits, purchases, fees -> for each: date, amount, new balance)
+    public transactionHistory();
     
-    //check if as savings account, if not charge fee
+    //connect to savings account
+    
+    //check if has savings account, if not charge fee
+    private boolean hasSavingsAccount; 
+    private double balance;
+    private double noSavingsFee = 25.0;
+    public void checkSavingsConnection() {
+        if (!hasSavingsAccount) {// if it is false, user is charged 
+            balance -= noSavingsFee;
+            System.out.println("you do not have a savings account. a fee of $25.00 has been charged. ");
+        }
+    }
     //create checking account
     //seaparate csv file for only checkings
     //more than one checking account -> unique checking account num separate from account num
-    
+    //2d array for csv file, nested loops (first for loop is row, nested loop is column)
+    // declare dimensions to create array (x rows, x columns) but must be dynamic array to allow it to expand
+    // don't hardcode length of array, use .length in loops? 
+    //search algorithms
+    //no interface
