85666e8e4b421566961675ff1d2e04a001a4b30f
Krish Patel <krish.patel34132@gmail.com>
2026-02-19 08:59:56 -0500

    Add files via upload

---
+++ DebitCard.java
@@ -0,0 +1,78 @@
+public class DebitCard {
+    // Card properties
+    private String DebitCardNumber;
+    private String pin;
+    private String linkedAccountId;
+    private boolean isActive;
+    
+    public DebitCard(String DebitCardNumber, String pin, String linkedAccountId) {
+        this.DebitcardNumber = DebitCardNumber;
+        this.pin = pin;
+        this.linkedAccountId = linkedAccountId;
+        this.isActive = true; 
+    }
+    
+    public boolean verifyPin(String enteredPin){
+        if(!isActive){
+            System.out.println("Error : This card has been closed");
+            return false;
+        }
+        if(this.pin.equals(enteredPin)){
+            System.out.println("PIN verified. Access Granted.");
+            return true;
+        }else {
+            System.out.println("Incorrect PIN");
+            return false;
+        }
+    }
+    
+    public String getDebitCardNumber() {
+        return cardNumber;
+    }
+    
+    public String getLinkedAccountId() {
+        return linkedAccountId;
+    }
+    
+    public boolean isActive() {
+        return isActive;
+    }
+    
+    public double checkBalance() {
+        if(!isActive) {
+            System.out.println("Error: Card is not active");
+            return 0.0;
+        }
+        System.out.println("Checking balance for account: " + linkedAccountId);
+        return 1000.00; //temporary
+    }
+    
+    public boolean withdraw(double amount) {
+        if(!isActive) {
+            System.out.println("Error: Card is not active");
+            return false;
+        }
+        System.out.println("Withdrawing $" + amount + " from account: " + linkedAccountId);
+        return true;
+        //replace this with code that calls the checking account team's withdraw method to actually remove money.
+        //later situation
+    }
+    
+    public boolean deposit(double amount) {
+        if(!isActive) {
+            System.out.println("Error: Card is not active");
+            return false;
+        }
+        System.out.println("Depositing $" + amount + " to account: " + linkedAccountId);
+        return true;
+    }
+    
+    public void closeCard() {
+        this.isActive = false;
+        System.out.println("Card " + cardNumber + " has been closed.");
+    }
+    
+    public static void main(String[] args) {
+        // optional: add demo code here to test later during standup
+    }
+}
