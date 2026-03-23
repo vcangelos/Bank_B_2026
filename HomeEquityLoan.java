c7eea37ff45ae0c654dd77a97f58da5e3018ab29
jordinr16 <jrivera229@obps.org>
2026-03-23 08:46:35 -0400

    Add files via upload

--- HomeEquityLoan.java
+++ HomeEquityLoan.java
@@ -1,12 +1,13 @@
 
 public class HomeEquityLoan {
     private double appraisedHomeValue; //house value
-    private double currentMortgageBalance;
+    private double currentMortgageBalance; //mortgage balance
     private double loanAmount; //total amount loaned
     private double availableEquity; //appraised value - mortgage balance
     
-    //fixed interest rate
-    private static final double interestRate;
+    //fixed interest rate and fees
+    private static final double interestRate = 0.03;
+    private static final double bankFees = 400.0;
     
     //loan term
     private int termMonths;
@@ -21,4 +22,40 @@ public class HomeEquityLoan {
 
     //progress tracking
     private int paymentsMade;
+    
+    //overloaded constructor
+    public HomeEquityLoan(double appraisedHomeValue, double currentMortgageBalance, double loanAmount, int termMonths, String accountType, double accountBalance) {
+        this.appraisedValue = appraisedValue;
+        this.currentMortgageBalance = currentMortgageBalance;
+        this.loanAmount = loanAmount;
+        this.termMonths = termMonths;
+        this.accountType = accountType;
+        this.accountBalance = accountBalance;
+        
+        this.availableEquity = appraisedValue - currentMortgageBalance;
+        this.remainingBalance = loanAmount + bankFees;
+        this.paymentsMade = 0;
+    }
+    
+    //validate eligiblity
+    public boolean isApproved() {
+        return appraisedValue > 0 && currentMortgageBalance >= 0 && loanAmount > 0 && loanAmount <= availableEquity && termMonths > 0;
+    }
+    
+    //calculate monthly payment
+    public void calculateMonthlyPayment() {
+        double monthlyRate = interestRate / 12;
+
+        monthlyPayment = (remainingBalance * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -termMonths));
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
