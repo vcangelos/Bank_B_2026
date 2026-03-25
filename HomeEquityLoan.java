d3bf850cd12340c51c549c3674c50ff6301a060b
jordinr16 <jrivera229@obps.org>
2026-03-25 08:20:41 -0400

    Add files via upload

--- HomeEquityLoan.java
+++ HomeEquityLoan.java
@@ -18,21 +18,27 @@ public class HomeEquityLoan {
 
     //payment source
     private String accountType;   //"checking" or "savings"
-    private double accountBalance;
 
     //progress tracking
     private int paymentsMade;
+    private double accountBalance;
+
     
     //overloaded constructor
-    public HomeEquityLoan(double appraisedHomeValue, double currentMortgageBalance, double loanAmount, int termMonths, String accountType, double accountBalance) {
+    public HomeEquityLoan(double appraisedHomeValue, double currentMortgageBalance, double loanAmount, int termMonths, BankingCSV.User user, String accountID, SavingsAccount savingsAccount, String accounType) {
         this.appraisedValue = appraisedValue;
         this.currentMortgageBalance = currentMortgageBalance;
         this.loanAmount = loanAmount;
         this.termMonths = termMonths;
+        
+        this.user = user;
+        this.accountID = accountID;
+        this.savingsAccount = savingsAccount;
         this.accountType = accountType;
-        this.accountBalance = accountBalance;
         
         this.availableEquity = appraisedValue - currentMortgageBalance;
+        
+        //fees added to loan balance
         this.remainingBalance = loanAmount + bankFees;
         this.paymentsMade = 0;
     }
@@ -49,12 +55,61 @@ public class HomeEquityLoan {
         monthlyPayment = (remainingBalance * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -termMonths));
     }
     
+    //this is for one payment
+    public void makePayment() {
+        if (remainingBalance <= 0) 
+            System.out.println("Loan already paid off.");
+            return;
+        
+
+        boolean success = false;
+
+        //CHECKING ACCOUNT
+        if (accountType.equalsIgnoreCase("checking")) {
+
+            for (BankingCSV.Account acc : user.accounts) {
+                if (acc.accountID.equals(accountID)) {
+
+                    if (acc.balance >= monthlyPayment) {
+                        user.withdraw(accountID, monthlyPayment);
+                        success = true;
+                    
+                    break;
+
+        //SAVINGS ACCOUNT
+             } else if (accountType.equalsIgnoreCase("savings")) {
+
+                if (savingsAccount != null) 
+                    success = savingsAccount.withdrawSavings(monthlyPayment);
+             }
+        }
+            
+        //If payment failed
+        if (!success) {
+            System.out.println("Payment failed: insufficient funds.");
+            return;
+        }
+
+        //Update loan balance
+        double interest = remainingBalance * (interestRate / 12);
+        double principal = monthlyPayment - interest;
+
+        remainingBalance -= principal;
+        paymentsMade++;
+
+        System.out.println("Loan Payment #" + paymentsMade);
+        System.out.println("Interest: " + interest);
+        System.out.println("Principal: " + principal);
+        System.out.println("Remaining Loan Balance: " + remainingBalance);
+    }
     
-    
-    
-    
-    
-    
+    //simulate all payments
+    public void simulatePayments() {
+        while (remainingBalance > 0) {
+            makePayment();
+        }
+        System.out.println("Loan fully paid!");
+    }
     
     
     
